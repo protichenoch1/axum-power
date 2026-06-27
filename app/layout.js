6f45195034315d7f86dbc7b7f1188bd368579c6e
protichenoch1 <protichenoch1@gmail.com>
2026-06-27 20:51:31 +0300

    Add BottomNav component to RootLayout

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import BottomNav from "../components/BottomNav";
 
 export const metadata = {
   title: "Axum Power",
@@ -7,7 +8,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <BottomNav />
+      </body>
     </html>
   );
 }
