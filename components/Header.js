8861dfa57f6e9a9ce1e41d8c8b49724d4240c45b
protichenoch1 <protichenoch1@gmail.com>
2026-06-27 20:54:30 +0300

    Add Header component with layout structure

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <div className="header">
+      
+      <div className="left">📧</div>
+
+      <div className="title">Axum Power</div>
+
+      <div className="right">EN</div>
+
+    </div>
+  );
+}
